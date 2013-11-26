a73f51f032be13c296c5a6fbc49f3db386b36a85
Connor Mendenhall <ecmendenhall@gmail.com>
2013-11-25 21:42:49 -0600

    Move custom packages to site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -88,4 +88,24 @@ node default {
     target => $boxen::config::repodir
   }
 
+  # custom packages
+  include skype
+  include mongodb
+  include vagrant
+  include iterm2::dev
+  include calibre
+  include chrome
+  include dropbox
+  include onyx
+  include adium
+  include quicksilver
+  include vlc
+  include onepassword
+  include spotify
+  include zsh
+  include transmission
+  include caffeine
+  include colloquy
+  include heroku
+
 }
